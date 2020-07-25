a8f3839c5b84b4cb6535a52e94c5ed7b209cefb9
gunarevuri <39848983+gunarevuri@users.noreply.github.com>
2020-07-25 17:10:15 +0530

    Update stage_redshift.py

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,5 +1,5 @@
 
-m airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
